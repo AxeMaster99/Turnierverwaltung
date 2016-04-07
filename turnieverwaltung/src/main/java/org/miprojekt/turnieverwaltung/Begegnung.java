43eedc9d30a8a5dcbd2705de2a43b6a6f227bb46
de024 <de024@r135-136.pool.mintern.mi.hdm-stuttgart.de>
2016-04-07 18:38:15 +0200

    unused files deleted

--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/Begegnung.java
+++
@@ -1,5 +0,0 @@
-package org.miprojekt.turnieverwaltung;
-
-public class Begegnung {
-
-}
--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/Trainer.java
+++
@@ -1,9 +0,0 @@
-package org.miprojekt.turnieverwaltung;
-
-public class Trainer extends Mannschaftsmitglied {
-
-	public Trainer(String vorname) {
-		super(vorname);
-	}
-	
-}
