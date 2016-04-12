2ab540db7b73c870b7a92f799db7970fefbff808
danelse <danelse@10.60.63.50>
2016-04-12 09:24:32 +0200

    JavaFX Window works now

--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/GUI.java
+++ turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/GUI.java
@@ -1,16 +1,11 @@
 package org.miprojekt.turnieverwaltung;
 
-import GUI.Hauptfenster;
-
 public class GUI implements ISteuerbar {
 	
 	private Steuerung steuerung;
 	
 	public GUI(Steuerung steuerung) {
 		this.steuerung = steuerung;
-		
-		new Hauptfenster();
-
 	}
 
 }
--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/Main.java
+++ turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/Main.java
@@ -1,14 +1,30 @@
 package org.miprojekt.turnieverwaltung;
 
-import GUI.Hauptfenster;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
-	
+
+public class Main extends Application {
 	
 	public static void main(String args[]) {
-				
 		new Steuerung();
-		 
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		stage.setTitle("Rocket Ligue Turnierverwaltung");
+		
+		StackPane root = new StackPane();
+		
+		Scene scene = new Scene(root, 800, 600);
+		
+		stage.setScene(scene);
+		stage.show();
+		
 	}
 	
 }
