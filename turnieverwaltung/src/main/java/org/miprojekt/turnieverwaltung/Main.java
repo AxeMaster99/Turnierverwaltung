4e0bfd505b46875d444fc0b10cdc615f8c8eb1e8
de024 <de024@hdm-stuttgart.de>
2016-04-12 09:07:35 +0200

    Bugs behoben

--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/Main.java
+++ turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/Main.java
@@ -5,13 +5,13 @@ import GUI.Hauptfenster;
 public class Main {
 	
 	
-	public static void main(int args[]) {
+	public static void main(String args[]) {
 		
 		System.out.println("ok");
 		
 		new Steuerung();
 		
-		new Hauptfenster().launch();
+		new Hauptfenster();
 		 
 	}
 	
