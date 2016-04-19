7cfebe5faea074f94352404b5c85277cba1023c5
danelse <danelse@10.60.39.249>
2016-04-19 11:59:34 +0200

    Submit and back button added

--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/windows/Mannschaftsnamen.java
+++ turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/windows/Mannschaftsnamen.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.KeyCode;
@@ -23,23 +24,28 @@ public class Mannschaftsnamen extends SceneParent {
 
 	private int anzahlMannschaften;
 	private int cnt = 0; // Globale Laufvariable
-	ProgressBar pb_fortschritt = new ProgressBar(0);
-	
+	private ProgressBar pb_fortschritt = new ProgressBar(0);
+	private ProgressIndicator pi = new ProgressIndicator(0);
+	private Button b_submit = new Button("OK");
+	private GridPane grid = new GridPane();
+	private Button b_add = new Button("hinzufügen");
+	private Button b_delete = new Button("entfernen");
+	private Button b_edit = new Button("Name ändern");
+	private Button b_back = new Button("zurück");
+
+	private TextField t_teamnames = new TextField();
+
 	public Mannschaftsnamen(Main main, int anzahlMannschaften) {
 		super(main);
 		this.anzahlMannschaften = anzahlMannschaften;
 
-		GridPane grid = new GridPane();
-		Button b_add = new Button("hinzufügen");
-		Button b_delete = new Button("entfernen");
-		Button b_edit = new Button("Name ändern");
-		TextField t_teamnames = new TextField();
-		
+		b_submit.setDisable(true);
+
 		ObservableList<String> teams = FXCollections.observableArrayList();
 		ListView<String> lb_teams = new ListView<String>(teams);
 
 		GridPane.setColumnSpan(lb_teams, 2);
-		GridPane.setColumnSpan(pb_fortschritt, 2);
+		GridPane.setColumnSpan(pb_fortschritt, 1);
 		pb_fortschritt.setMinWidth(400);
 
 		grid.add(t_teamnames, 0, 0);
@@ -47,7 +53,10 @@ public class Mannschaftsnamen extends SceneParent {
 		grid.add(b_delete, 0, 2);
 		grid.add(b_edit, 1, 2);
 		grid.setPadding(new Insets(25));
-		grid.add(pb_fortschritt, 0, 3);
+		grid.add(pb_fortschritt, 1, 3);
+		grid.add(pi, 0, 3);
+		grid.add(b_submit, 3, 4);
+		grid.add(b_back, 0, 4);
 
 		GridPane.setMargin(t_teamnames, new Insets(0, 25, 0, 0));
 
@@ -71,7 +80,7 @@ public class Mannschaftsnamen extends SceneParent {
 						Alert alert = new Alert(AlertType.ERROR);
 						alert.setTitle("Fehler");
 						alert.setHeaderText("Es ist ein Fehler aufgetreten");
-						alert.setContentText("Mannshaft ist schon vorhanden");
+						alert.setContentText("Mannschaft ist schon vorhanden");
 						alert.showAndWait();
 						teamSchonVorhanden = true;
 						break;
@@ -105,8 +114,8 @@ public class Mannschaftsnamen extends SceneParent {
 			if (lb_teams.getSelectionModel().getSelectedItem() == null) {
 				Alert alert = new Alert(AlertType.ERROR);
 				alert.setTitle("Fehler");
-				alert.setHeaderText("Kein Mannschaft ausgewählt");
-				alert.setContentText("!");
+				alert.setHeaderText("Es ist ein Fehler aufgetreten");
+				alert.setContentText("Keine Mannschaft ausgewählt");
 				alert.showAndWait();
 			} else {
 				TextInputDialog dialog = new TextInputDialog(lb_teams.getSelectionModel().getSelectedItem());
@@ -115,8 +124,16 @@ public class Mannschaftsnamen extends SceneParent {
 				dialog.setContentText("Neuer Name:");
 				Optional<String> result = dialog.showAndWait();
 				if (result.isPresent()) {
-					teams.set(lb_teams.getSelectionModel().getSelectedIndex(), result.get());
-					System.out.println("Neuer Name: " + result.get());
+					if (result.get().equals("")) {
+						Alert alert = new Alert(AlertType.ERROR);
+						alert.setTitle("Fehler");
+						alert.setHeaderText("Es ist ein Fehler aufgetreten");
+						alert.setContentText("Name darf nicht leer sein");
+						alert.showAndWait();
+					} else {
+						teams.set(lb_teams.getSelectionModel().getSelectedIndex(), result.get());
+						System.out.println("Neuer Name: " + result.get());
+					}
 				}
 			}
 		});
@@ -126,17 +143,30 @@ public class Mannschaftsnamen extends SceneParent {
 				b_add.fire();
 			}
 		});
+		
+		b_back.setOnAction((event) -> {
+			main.getStage().setScene(main.getScreen("settings"));
+		});
 
 		grid.add(lb_teams, 0, 1);
 		GridPane.setMargin(lb_teams, new Insets(25, 0, 0, 0));
 		this.getChildren().add(grid);
 	}
-	
-	
+
 	public void updateFortschritt() {
 		double cnt_double = cnt;
 		double anzMannschaftenDouble = anzahlMannschaften;
-		this.pb_fortschritt.setProgress(1/anzMannschaftenDouble*cnt);
+		this.pb_fortschritt.setProgress(1 / anzMannschaftenDouble * cnt);
+		this.pi.setProgress(this.pb_fortschritt.getProgress());
+		if (cnt == anzahlMannschaften) {
+			b_submit.setDisable(false);
+			b_add.setDisable(true);
+			t_teamnames.setDisable(true);
+		} else {
+			b_submit.setDisable(true);
+			b_add.setDisable(false);
+			t_teamnames.setDisable(false);
+		}
 	}
 
 }
