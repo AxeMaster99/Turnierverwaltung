c50ec545da6f97cd61d0ed0afb8b208a5d03293f
danelse <danelse@100.67.112.4>
2016-05-11 17:59:08 +0200

    pane added

--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/windows/SpielBaum.java
+++ turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/windows/SpielBaum.java
@@ -6,6 +6,9 @@ import org.miprojekt.turnieverwaltung.Main;
 import org.miprojekt.turnieverwaltung.gui.SceneParent;
 
 import backend.Match;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class SpielBaum extends SceneParent {
 
@@ -20,6 +23,28 @@ public class SpielBaum extends SceneParent {
 		for (int i = 0; i < this.matches.size(); i++) {
 			System.out.print("Begegnung "+ (i+1) + ": " + matches.get(i).getMannschaft1().getName() + " gegen ");
 			System.out.println(matches.get(i).getMannschaft2().getName());
+		}
+		
+
+		for(int i = 0; i < steuerung.getMatches().size(); i++) {
+			System.out.println("Begegnung Nr. "+(i+1));
+			System.out.println("m1:" + steuerung.getMatches().get(i).getMannschaft1().getName());
+			System.out.println("m1:" + steuerung.getMatches().get(i).getMannschaft2().getName());
+			System.out.println();
+		}
+		
+		for (int i = 0; i < steuerung.getMatches().size(); i++) {
+			Pane p = new Pane();
+			// p.setStyle("-fx-background-color: white");
+			p.setTranslateX(10);
+			p.setTranslateY(i*50);
+			GridPane grid = new GridPane();
+			Label m1label = new Label(steuerung.getMatches().get(i).getMannschaft1().getName());
+			Label m2label = new Label(steuerung.getMatches().get(i).getMannschaft2().getName());
+			grid.add(m1label, 0, 0);
+			grid.add(m2label, 0, 1);
+			p.getChildren().add(grid);
+			this.getChildren().add(p);
 		}
 
 	}
