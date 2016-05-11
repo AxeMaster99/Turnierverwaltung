087501ffc96cdd1149aeb2acf2698f1bed3eb306
Axel Forstenhäusler <af073@hdm-stuttgart.de>
2016-05-11 18:02:58 +0200

    SpielBaumScreen working

--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/windows/SpielBaum.java
+++ turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/windows/SpielBaum.java
@@ -21,26 +21,20 @@ public class SpielBaum extends SceneParent {
 		System.out.println(this.matches.size());
 
 		for (int i = 0; i < this.matches.size(); i++) {
-			System.out.print("Begegnung "+ (i+1) + ": " + matches.get(i).getMannschaft1().getName() + " gegen ");
-			System.out.println(matches.get(i).getMannschaft2().getName());
-		}
-		
-
-		for(int i = 0; i < steuerung.getMatches().size(); i++) {
-			System.out.println("Begegnung Nr. "+(i+1));
-			System.out.println("m1:" + steuerung.getMatches().get(i).getMannschaft1().getName());
-			System.out.println("m1:" + steuerung.getMatches().get(i).getMannschaft2().getName());
+			System.out.println("Begegnung Nr. " + (i + 1));
+			System.out.println("m1:" + this.matches.get(i).getMannschaft1().getName());
+			System.out.println("m2:" + this.matches.get(i).getMannschaft2().getName());
 			System.out.println();
 		}
-		
-		for (int i = 0; i < steuerung.getMatches().size(); i++) {
+
+		for (int i = 0; i < this.matches.size(); i++) {
 			Pane p = new Pane();
 			// p.setStyle("-fx-background-color: white");
 			p.setTranslateX(10);
-			p.setTranslateY(i*50);
+			p.setTranslateY(i * 50);
 			GridPane grid = new GridPane();
-			Label m1label = new Label(steuerung.getMatches().get(i).getMannschaft1().getName());
-			Label m2label = new Label(steuerung.getMatches().get(i).getMannschaft2().getName());
+			Label m1label = new Label(this.matches.get(i).getMannschaft1().getName());
+			Label m2label = new Label(this.matches.get(i).getMannschaft2().getName());
 			grid.add(m1label, 0, 0);
 			grid.add(m2label, 0, 1);
 			p.getChildren().add(grid);
