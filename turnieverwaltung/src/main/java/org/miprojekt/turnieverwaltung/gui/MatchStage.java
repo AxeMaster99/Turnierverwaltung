24b2dcfd236aa86e5c00dfd29d4b679e3d75b6d1
Axel Forstenhäusler <af073@hdm-stuttgart.de>
2016-05-14 12:57:03 +0200

    bester Timer

--- turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/MatchStage.java
+++ turnieverwaltung/src/main/java/org/miprojekt/turnieverwaltung/gui/MatchStage.java
@@ -1,7 +1,13 @@
 package org.miprojekt.turnieverwaltung.gui;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
 import interfaces.IMatch;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -15,13 +21,22 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class MatchStage extends Stage {
 
 	private IMatch match;
+
+	Timeline timeline;
+	private int timerdauer = 10;
+
 	private Label l_Spielstand = new Label();
 	private Label l_Mannschaft1 = new Label();
 	private Label l_Mannschaft2 = new Label();
+
+	private Label l_timer = new Label("Verbleibende Zeit: ");
+	private Label l_timerdauer = new Label(Integer.toString(timerdauer));
+
 	private Button b_TorMannschaft1 = new Button("Tor M1");
 	private Button b_TorMannschaft2 = new Button("Tor M2");
 	private Button b_Start_Stopp = new Button("Start");
@@ -30,9 +45,10 @@ public class MatchStage extends Stage {
 		super();
 		this.match = match;
 
+		
 		Pane root = new Pane();
 		GridPane grid = new GridPane();
-		grid.setMinWidth(300);
+		grid.setMinWidth(350);
 		grid.setMinHeight(200);
 		grid.setAlignment(Pos.CENTER);
 
@@ -44,18 +60,19 @@ public class MatchStage extends Stage {
 
 		Font font = new Font(25);
 
+		l_timerdauer.setFont(font);
+		
 		l_Spielstand.setFont(font);
 		l_Spielstand.setText(this.match.getToreM1() + ":" + this.match.getToreM2());
 
-		
 		ColumnConstraints col1 = new ColumnConstraints(100);
 		ColumnConstraints col2 = new ColumnConstraints(100);
 		ColumnConstraints col3 = new ColumnConstraints(100);
 		grid.setHgap(10);
 		grid.setVgap(10);
-		
-		grid.getColumnConstraints().addAll(col1,col2,col3);
-		
+
+		grid.getColumnConstraints().addAll(col1, col2, col3);
+
 		grid.add(l_Mannschaft1, 0, 0);
 		GridPane.setHalignment(l_Mannschaft1, HPos.RIGHT);
 
@@ -70,13 +87,20 @@ public class MatchStage extends Stage {
 
 		grid.add(b_Start_Stopp, 1, 1);
 		GridPane.setHalignment(b_Start_Stopp, HPos.CENTER);
-		
+
 		grid.add(b_TorMannschaft2, 2, 1);
 		GridPane.setHalignment(b_TorMannschaft2, HPos.LEFT);
-		
+
+		grid.add(l_timer, 0, 2);
+		GridPane.setHalignment(l_timer, HPos.LEFT);
+
+		grid.add(l_timerdauer, 1, 2);
+		GridPane.setHalignment(l_timerdauer, HPos.CENTER);
+
+		// grid.setGridLinesVisible(true);
 		root.getChildren().add(grid);
 
-		Scene scene = new Scene(root, 300, 200);
+		Scene scene = new Scene(root, 350, 200);
 
 		this.setTitle(this.match.getMannschaft1().getName() + " vs " + this.match.getMannschaft2().getName());
 		this.setScene(scene);
@@ -94,20 +118,44 @@ public class MatchStage extends Stage {
 
 		b_Start_Stopp.setOnAction((event) -> {
 			if (b_Start_Stopp.getText().equals("Stop")) {
-				
-				
-				
+
+				this.stoppeSpiel();
 				b_Start_Stopp.setText("Start");
 				b_TorMannschaft1.setDisable(true);
 				b_TorMannschaft2.setDisable(true);
 
 			} else {
+				this.starteSpiel();
 				b_Start_Stopp.setText("Stop");
 				b_TorMannschaft1.setDisable(false);
 				b_TorMannschaft2.setDisable(false);
 			}
 		});
 
+	}
+
+	private void stoppeSpiel() {
+		timeline.stop();
+	}
+
+	private void starteSpiel() {
+		timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+			timerdauer--;
+			l_timerdauer.setText(Integer.toString(timerdauer));
+			if (timerdauer == 0) {
+				b_Start_Stopp.setText("Start");
+				b_Start_Stopp.setDisable(true);
+				b_TorMannschaft1.setDisable(true);
+				b_TorMannschaft2.setDisable(true);
+				this.speichereErgebnis();
+			}
+		}));
+		timeline.setCycleCount(timerdauer);
+		timeline.play();
+	}
+
+	private void speichereErgebnis() {
+		// TODO IMPLEMENT!
 	}
 
 }
