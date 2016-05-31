64469312d4b3de4f5406b2835a7796850ab3b5c2
Axel Forstenhäusler <af073@hdm-stuttgart.de>
2016-05-31 11:20:21 +0200

    beim letzen wurde nicht alles übertragen

---
+++ turnieverwaltung/src/main/java/stages/GroupMatchStage.java
@@ -0,0 +1,169 @@
+package stages;
+
+import java.util.Optional;
+
+import interfaces.IMatch;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+import javafx.util.Duration;
+import panes.GroupPane;
+import stages.MatchStage.Event;
+import threads.GUIUpdateThread;
+import verwaltung.Steuerung;
+
+public class GroupMatchStage extends MatchStage {
+
+	public enum Status {
+		closed, opened, hidden, started, stopped, finished
+	}
+
+	private GroupPane groupPane;
+	private Status currentState = Status.closed;
+
+	public GroupMatchStage(GroupPane groupPane, IMatch match) {
+		super(match);
+		this.groupPane = groupPane;
+
+		this.setOnCloseRequest((WindowEvent) -> {
+
+			if (this.currentState == Status.opened) {
+				this.switchState(Event.close);
+			} else {
+				this.stoppeSpiel();
+				Alert alert = new Alert(AlertType.CONFIRMATION);
+				alert.setTitle("Warnung");
+				alert.setHeaderText("Wirklich schließen?");
+				alert.setContentText(
+						"Wollen sie das Fenster wirklich schließen? Der Timer wird dann gestoppt und das Spiel muss später fortgesetzt werden.");
+				Optional<ButtonType> result = alert.showAndWait();
+				if (result.get() == ButtonType.OK) {
+					this.switchState(Event.close);
+					this.close();
+				} else {
+					WindowEvent.consume();
+					alert.close();
+					this.starteSpiel();
+				}
+			}
+		});
+
+		b_Start_Stopp.setOnAction((event) -> {
+			this.switchState(Event.start_stop);
+		});
+
+		b_hide.setOnAction((event) -> {
+			this.switchState(Event.hide);
+		});
+	}
+
+	private void starteSpiel() {
+
+		if (matchTimer == -1) {
+			matchTimer = timerdauer;
+		}
+		this.timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+			matchTimer--;
+			l_timerdauer.setText(String.format("%02d:%02d", matchTimer / 60, matchTimer % 60));
+		}));
+
+		timeline.setOnFinished((e) -> {
+			this.switchState(Event.timer_finished);
+			this.groupPane.getTable().refresh();
+		});
+
+		timeline.setCycleCount(matchTimer);
+		timeline.play();
+	}
+
+	public void beendeSpiel() {
+		this.close();
+		this.match.setSieger();
+		Platform.runLater(new Runnable() {
+
+			@Override
+			public void run() {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Information");
+				alert.setHeaderText("Spiel Nr." + match.getIndex() + " ist beendet.\n(" + match.getMannschaft1() + " vs "
+						+ match.getMannschaft2() + ")");
+				alert.setContentText("Das Spiel wurde Beendet. " + (match.getUnentschieden()? "Es endete Unentschieden, die Punkte werden verteilt." : ("Gewonnen hat: ")+match.getSieger()));
+				alert.showAndWait();
+			}
+		});
+	}
+	
+	public void switchState(Event e) {
+		switch (currentState) {
+		case closed:
+			if (e == Event.click) {
+				this.show();
+				this.currentState = Status.opened;
+			}
+			break;
+		case finished:
+			break;
+		case hidden:
+			if (e == Event.click) {
+				this.show();
+				this.currentState = Status.opened;
+			}
+			break;
+		case opened:
+			if (e == Event.start_stop) {
+				this.starteSpiel();
+				b_Start_Stopp.setText("Stop");
+				b_TorMannschaft1.setDisable(false);
+				b_TorMannschaft2.setDisable(false);
+				this.currentState = Status.started;
+			} else if (e == Event.hide) {
+				this.hide();
+				this.currentState = Status.hidden;
+			} else if (e == Event.close) {
+				this.currentState = Status.closed;
+				this.close();
+			}
+			break;
+		case started:
+			if (e == Event.start_stop) {
+				this.stoppeSpiel();
+				b_Start_Stopp.setText("Start");
+				b_TorMannschaft1.setDisable(true);
+				b_TorMannschaft2.setDisable(true);
+				this.currentState = Status.stopped;
+			} else if (e == Event.timer_finished) {
+				this.beendeSpiel();
+				this.close();
+				this.currentState = Status.finished;
+			} else if (e == Event.hide) {
+				this.hide();
+				this.currentState = Status.hidden;
+			} else if (e == Event.close) {
+				this.currentState = Status.closed;
+				this.close();
+			}
+			break;
+		case stopped:
+			if (e == Event.start_stop) {
+				this.starteSpiel();
+				b_Start_Stopp.setText("Stop");
+				b_TorMannschaft1.setDisable(false);
+				b_TorMannschaft2.setDisable(false);
+				this.currentState = Status.started;
+			} else if (e == Event.hide) {
+				this.hide();
+				this.currentState = Status.hidden;
+			} else if (e == Event.close) {
+				this.currentState = Status.closed;
+				this.close();
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+}
---
+++ turnieverwaltung/src/main/java/stages/TreeMatchStage.java
@@ -0,0 +1,223 @@
+package stages;
+
+import java.util.Optional;
+
+import interfaces.IMatch;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+import javafx.util.Duration;
+import panes.MatchPane;
+import threads.GUIUpdateThread;
+
+public class TreeMatchStage extends MatchStage {
+
+	public enum Status {
+		clickable_white, unclickable_white, clickable_yellow, unclickable_yellow, clickable_orange, unclickable_orange, finished_green, hovered
+	}
+
+	private Status currentState = Status.clickable_white;
+
+	public TreeMatchStage(IMatch match, MatchPane matchPane) {
+		super(match, matchPane);
+
+		this.setOnCloseRequest((WindowEvent) -> {
+
+			if (this.currentState == Status.unclickable_white || this.currentState == Status.unclickable_orange) {
+				this.switchState(Event.close);
+			} else {
+				this.stoppeSpiel();
+				Alert alert = new Alert(AlertType.CONFIRMATION);
+				alert.setTitle("Warnung");
+				alert.setHeaderText("Wirklich schließen?");
+				alert.setContentText(
+						"Wollen sie das Fenster wirklich schließen? Der Timer wird dann gestoppt und das Spiel muss später fortgesetzt werden.");
+				Optional<ButtonType> result = alert.showAndWait();
+				if (result.get() == ButtonType.OK) {
+					this.switchState(Event.close);
+					this.close();
+				} else {
+					WindowEvent.consume();
+					alert.close();
+					this.starteSpiel();
+				}
+			}
+		});
+
+		b_Start_Stopp.setOnAction((event) -> {
+			this.switchState(Event.start_stop);
+		});
+
+		b_hide.setOnAction((event) -> {
+			this.switchState(Event.hide);
+		});
+	}
+	
+	private void starteSpiel() {
+
+		if (matchTimer == -1) {
+			matchTimer = timerdauer;
+		}
+		this.timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+			matchTimer--;
+			l_timerdauer.setText(String.format("%02d:%02d", matchTimer / 60, matchTimer % 60));
+		}));
+
+		timeline.setOnFinished((e) -> {
+			if (this.match.getToreM1() == this.match.getToreM2()) {
+				Platform.runLater(new Runnable() {
+
+					@Override
+					public void run() {
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("Information");
+						alert.setHeaderText("Unentschieden");
+						alert.setContentText(
+								"Spiel " + match.getIndex() + " endete Unentschieden. Das nächste Tor entscheidet.");
+						alert.showAndWait();
+						switchState(Event.click);
+					}
+				});
+				new GUIUpdateThread(match, this).start();
+			} else {
+				new GUIUpdateThread(match, this).start();
+			}
+			this.matchPane.setLabelErgebnis(this.match.getToreM1(), this.match.getToreM2());
+		});
+
+		timeline.setCycleCount(matchTimer);
+		timeline.play();
+	}
+	
+	public void beendeSpiel() {
+		this.close();
+		this.match.setSieger();
+		Platform.runLater(new Runnable() {
+
+			@Override
+			public void run() {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Information");
+				alert.setHeaderText("Spiel Nr." + match.getIndex() + " ist beendet.\n(" + match.getSieger() + " vs "
+						+ match.getVerlierer() + ")");
+				alert.setContentText("Das Spiel wurde Beendet. Gewonnen hat: " + match.getSieger());
+				alert.showAndWait();
+			}
+		});
+
+		this.matchPane.getSteuerung().getRangStage().updateTable();
+		this.matchPane.setDisable(false);
+		this.matchPane.getSteuerung().updateSpielBaum();
+	}
+
+	public void switchState(Event e) {
+		switch (currentState) {
+		case clickable_orange:
+			if (e == Event.click) {
+				this.matchPane.setDisable(true);
+				currentState = Status.unclickable_orange;
+			}
+			break;
+		case clickable_white:
+			if (e == Event.click) {
+				this.matchPane.setDisable(true);
+				currentState = Status.unclickable_white;
+			}
+			break;
+		case clickable_yellow:
+			if (e == Event.click) {
+				this.show();
+				this.matchPane.setDisable(true);
+				currentState = Status.unclickable_yellow;
+			} else if (e == Event.timer_finished) {
+				this.matchPane.getGrid()
+						.setStyle("-fx-background-color: rgba(127,255,0,1);"
+								+ "-fx-background-radius: 5;-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"
+								+ "-fx-border-color:black;" + "-fx-border-radius:5;");
+				this.currentState = Status.finished_green;
+			}
+			break;
+		case finished_green:
+			break;
+		case unclickable_orange:
+			if (e == Event.start_stop) {
+				this.starteSpiel();
+				this.matchPane.getGrid()
+						.setStyle("-fx-background-color: yellow;"
+								+ "-fx-background-radius: 5;-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"
+								+ "-fx-border-color:black;" + "-fx-border-radius:5;");
+				b_TorMannschaft1.setDisable(false);
+				b_TorMannschaft2.setDisable(false);
+				b_Start_Stopp.setText("Stop");
+				this.currentState = Status.unclickable_yellow;
+			} else if (e == Event.close) {
+				this.matchPane.setDisable(false);
+				this.currentState = Status.clickable_orange;
+			} else if (e == Event.hide) {
+				this.hide();
+				this.matchPane.setDisable(false);
+				this.currentState = Status.clickable_orange;
+			}
+			break;
+		case unclickable_white:
+			if (e == Event.close) {
+				this.close();
+				this.matchPane.setDisable(false);
+				this.currentState = Status.clickable_white;
+			} else if (e == Event.start_stop) {
+				this.starteSpiel();
+				this.matchPane.getGrid()
+						.setStyle("-fx-background-color: yellow;"
+								+ "-fx-background-radius: 5;-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"
+								+ "-fx-border-color:black;" + "-fx-border-radius:5;");
+				b_TorMannschaft1.setDisable(false);
+				b_TorMannschaft2.setDisable(false);
+				b_Start_Stopp.setText("Stop");
+				this.currentState = Status.unclickable_yellow;
+			} else if (e == Event.hide) {
+				this.hide();
+				this.matchPane.setDisable(false);
+				this.currentState = Status.clickable_white;
+			}
+			break;
+		case unclickable_yellow:
+			if (e == Event.close) {
+				this.matchPane.getGrid()
+						.setStyle("-fx-background-color: orange;"
+								+ "-fx-background-radius: 5;-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"
+								+ "-fx-border-color:black;" + "-fx-border-radius:5;");
+				b_Start_Stopp.setText("Start");
+				this.matchPane.setDisable(false);
+				b_TorMannschaft1.setDisable(true);
+				b_TorMannschaft2.setDisable(true);
+				this.currentState = Status.clickable_orange;
+			} else if (e == Event.start_stop) {
+				this.matchPane.getGrid()
+						.setStyle("-fx-background-color: orange;"
+								+ "-fx-background-radius: 5;-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"
+								+ "-fx-border-color:black;" + "-fx-border-radius:5;");
+				this.stoppeSpiel();
+				b_Start_Stopp.setText("Start");
+				b_TorMannschaft1.setDisable(true);
+				b_TorMannschaft2.setDisable(true);
+				this.currentState = Status.unclickable_orange;
+			} else if (e == Event.timer_finished) {
+				this.matchPane.getGrid()
+						.setStyle("-fx-background-color: rgba(127,255,0,1);"
+								+ "-fx-background-radius: 5;-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);"
+								+ "-fx-border-color:black;" + "-fx-border-radius:5;");
+				this.currentState = Status.finished_green;
+			} else if (e == Event.hide) {
+				this.hide();
+				this.matchPane.setDisable(false);
+				this.currentState = Status.clickable_yellow;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
