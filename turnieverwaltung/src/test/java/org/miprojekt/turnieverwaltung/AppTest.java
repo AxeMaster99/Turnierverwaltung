4abcd33b433bc23da403ba3e3446a70198d9382c
Enzo <vp019@hdm-stuttgart.de>
2016-06-18 21:10:57 +0200

    Added two more JUTests to Matches (gesamteKlasse) and Groups (ArrayList)

--- turnieverwaltung/src/test/java/org/miprojekt/turnieverwaltung/AppTest.java
+++ turnieverwaltung/src/test/java/org/miprojekt/turnieverwaltung/AppTest.java
@@ -25,7 +25,7 @@ public class AppTest extends Application {
     public void testApp() {
         Assert.assertTrue( true );
     }
-    
+//	Tests fuer Gruppenspiele   
    	@Test
     public void testeGruppenSieger() {
    		
@@ -76,7 +76,58 @@ public class AppTest extends Application {
     	Platform.exit();
    		PlatformImpl.exit();
     }
+   	
+//	Tests fuer Gruppenspiele "Rangliste" ueberpruefen 
+   	@Test
+    public void testeGruppenArrayList() {
+   		
+    	Mannschaft m1 = new Mannschaft("Mannschaft 1");
+    	Mannschaft m2 = new Mannschaft("Mannschaft 2");
+    	Mannschaft m3 = new Mannschaft("Mannschaft 3");
+    	Mannschaft m4 = new Mannschaft("Mannschaft 4");
+    	
+    	Group gruppe = new Group(m1, m2, m3, m4);
+    	
+    	IMatch matches[] = new Match[6];
+    	
+    	matches[0] = MatchFactory.build(m1, m2);
+    	matches[1] = MatchFactory.build(m2, m3);
+    	matches[2] = MatchFactory.build(m3, m4);
+    	matches[3] = MatchFactory.build(m1, m3);
+    	matches[4] = MatchFactory.build(m2, m4);
+    	matches[5] = MatchFactory.build(m1, m4);
 
+    	matches[0].setToreM1(2);
+       	matches[0].setToreM2(4);
+    	
+    	matches[1].setToreM1(10);
+    	matches[1].setToreM2(0);
+    	
+    	matches[2].setToreM1(3);
+    	matches[2].setToreM2(1);
+    	
+    	matches[3].setToreM1(1);
+    	matches[3].setToreM2(4);
+
+    	matches[4].setToreM1(2);
+    	matches[4].setToreM2(11);
+    	
+    	matches[5].setToreM1(3);
+    	matches[5].setToreM2(0);
+
+    	matches[0].setSieger();
+    	matches[1].setSieger();
+    	matches[2].setSieger();
+    	matches[3].setSieger();
+    	matches[4].setSieger();
+    	matches[5].setSieger();
+  	
+    	Assert.assertEquals(m2.getName(), gruppe.getGruppenSieger().get(0));
+    	Assert.assertEquals(m3.getName(), gruppe.getGruppenSieger().get(1));
+    	Assert.assertEquals(m4.getName(), gruppe.getGruppenSieger().get(2));
+    	Assert.assertEquals(m1.getName(), gruppe.getGruppenSieger().get(3));
+    	
+    }
 //	Tests fuer Match (Sieger, Verlierer)
    	@Test
     public void testeMatchSiegerM1() throws Exception {
@@ -148,6 +199,31 @@ public class AppTest extends Application {
     	
     	Assert.assertEquals(matches[0].getToreM1(), 1);
     	Assert.assertEquals(matches[0].getToreM2(), 1);
+   	}
+ 	
+ 	@Test
+    public void gesamteKlasseMatch() throws Exception {
+   		Mannschaft m1 = new Mannschaft("Mannschaft 1");
+    	Mannschaft m2 = new Mannschaft("Mannschaft 2");
+    	IMatch matches[] = new Match[1];
+    	matches[0] = MatchFactory.build(m1, m2);
+    	matches[0].setTurnierType("KO-Turnier");
+    	
+    	matches[0].setToreM1(0);
+    	matches[0].setToreM2(0);
+    	matches[0].incrementToreM1();
+    	matches[0].incrementToreM2();
+    	matches[0].incrementToreM1();
+    	matches[0].incrementToreM1();
+    	
+    	matches[0].setSieger();
+    	
+    	Assert.assertEquals(matches[0].getToreM1(), 3);
+    	Assert.assertEquals(matches[0].getToreM2(), 1);
+    	Assert.assertFalse(matches[0].getUnentschieden());
+    	Assert.assertTrue(matches[0].isGameFinished());
+    	Assert.assertEquals(matches[0].getSieger(), m1);
+    	Assert.assertEquals(matches[0].getVerlierer(), m2);
    	}
  	
 	@Override
